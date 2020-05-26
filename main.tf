e3ea8bf4cb4ff76f728ae79a1d8fb42ae0753f69
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2020-05-26 14:46:00 +1000

    added input/output variables and variables

--- main.tf
+++ main.tf
@@ -3,16 +3,41 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+    resource_location = "australiaeast"
+}
+
+variable "environment_subnets" {
+    type = list
+    default = ["public", "application", "data"]
+}
+
+variable "VMSize_map" {
+    type = map
+    default = {
+        "development" = "Standard_A1_v2"
+        "staging" = "Standard_A2_v2"
+        "production" = "Standard_A4_v2"
+    }
+}
+
+resource "random_password" "vmpassword" {
+    length = 16
+    special = true
+    min_special = 5
+}
+
+
 #create the resource group
 resource "azurerm_resource_group" "rg" {
     name = "ateam-resource-group"
-    location = "australiaeast"
+    location = local.resource_location
 }
 
 #create the virtual network
 resource "azurerm_virtual_network" "vnet1" {
     resource_group_name = azurerm_resource_group.rg.name
-    location = "australiaeast"
+    location = local.resource_location
     name = "dev"
     address_space = ["10.0.0.0/16"]
 }
@@ -21,20 +46,20 @@ resource "azurerm_virtual_network" "vnet1" {
 resource "azurerm_subnet" "subnet1" {
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet1.name
-    name = "devsubnet"
+    name = var.environment_subnets[0]
     address_prefixes = ["10.0.0.0/24"]
 }
 
 ##create the network interface for the VM
 resource "azurerm_public_ip" "pub_ip" {
     name = "vmpubip"
-    location = "australiaeast"
+    location = local.resource_location
     resource_group_name = azurerm_resource_group.rg.name
     allocation_method = "Dynamic"
 }
 
 resource "azurerm_network_interface" "vmnic" {
-    location = "australiaeast"
+    location = local.resource_location
     resource_group_name = azurerm_resource_group.rg.name
     name = "vmnic1"
 
@@ -52,10 +77,10 @@ resource "azurerm_network_interface" "vmnic" {
 ##create the actual VM
 resource "azurerm_windows_virtual_machine" "devvm" {
     name = "development-vm"
-    location = "australiaeast"
-    size = "Standard_A1_v2"
+    location = local.resource_location
+    size = var.VMSize_map["development"]
     admin_username = "pradeep"
-    admin_password = "kq7UciQluJt%3dtj"
+    admin_password = random_password.vmpassword.result
     resource_group_name = azurerm_resource_group.rg.name
 
     network_interface_ids = [azurerm_network_interface.vmnic.id]
@@ -74,5 +99,17 @@ resource "azurerm_windows_virtual_machine" "devvm" {
 
 }
 ##end creating VM
+data "azurerm_public_ip" "vmip" {
+    name = azurerm_public_ip.pub_ip.name
+    resource_group_name = azurerm_windows_virtual_machine.devvm.resource_group_name
+}
+
+output "generated_password" {
+    value = random_password.vmpassword.result
+}
+
+output "public_ip_address" {
+    value = data.azurerm_public_ip.vmip
+}
 
 
